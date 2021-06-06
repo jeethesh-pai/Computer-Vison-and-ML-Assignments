ca16184fc1a08e219e0bbca8ab4a2eb7ff0f4fd6
Jeethesh Pai Umesh <j.umesh@tu-bs.de>
2021-06-06 12:35:26 +0200

    Task 5 completed

--- sheet4/tf_object_detection.py
+++ sheet4/tf_object_detection.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
-#
 # Task 4
 #
 # Load and use a pretrained `Object Detection` model from the `TensorFlow Model Garden`
@@ -10,18 +5,26 @@
 # Download/cache required model and test data
 import tensorflow as tf
 import os
+import time
 from utils import *
 from tf_utils import *
 import cv2
 import numpy as np
-from object_detection.utils import visualization_utils as viz_utils
+from models.research.object_detection.utils import visualization_utils as viz_utils
 import matplotlib
 import matplotlib.pyplot as plt
 from object_detection.utils import label_map_util
 
+tf.get_logger().setLevel('ERROR')
+
+model_name = input('model name: 1 - centernet Hourglass, 2 - centernetResnet50 ')
 print('Download model...')
-modelPath = download_model('20200713', 'centernet_hg104_1024x1024_coco17_tpu-32')
-print(modelPath)
+if int(model_name) == 1:
+    modelPath = download_model('20200713', 'centernet_hg104_1024x1024_coco17_tpu-32')
+    print(modelPath)
+else:
+    modelPath = download_model('20200711', 'centernet_resnet50_v1_fpn_512x512_kpts_coco17_tpu-8')
+    print(modelPath)
 
 print('Download labels...')
 labelsPath = download_labels('mscoco_label_map.pbtxt')
@@ -29,25 +32,18 @@ print(labelsPath)
 
 print('Download test images...')
 imagePaths = download_test_images(['image1.jpg', 'image2.jpg'])
-print('\n'.join(imagePaths))
+print('/n'.join(imagePaths))
 
 # Load the model
 
-
-print('Load model')
-
-# TODO: Load the downloaded saved tensorflow model using `load_model(..)` from the keras module
+print('Loading model...')
 savedModelPath = os.path.join(modelPath, "saved_model")
-model = REPLACE_THIS_MODEL
-#
-# ???
-#
+start_time = time.time()
+model = tf.saved_model.load(savedModelPath)
+end_time = time.time()
+print('Time taken to load the model:%.2f secs' % (end_time - start_time))
 
-
-# Load label map data (for plotting)
-
-print('Load labels')
-
+# Loading label_map data
 category_index = label_map_util.create_category_index_from_labelmap(labelsPath, use_display_name=True)
 
 # Run inference
@@ -57,23 +53,33 @@ matplotlib.use('TkAgg')  # Reactivate GUI backend (deactivated by `import viz_ut
 imgs = []
 for image_path in imagePaths:
     print('Running inference for {}... '.format(image_path))
-    img = cv2.imread(image_path)
+    image = cv2.imread(image_path)
+    img = tf.convert_to_tensor(image, dtype=tf.uint8)
+    img = tf.expand_dims(img, axis=0)
+    start_predict_time = time.time()
+    detections = model(img)
+    end_predict_time = time.time()
+    print('Time taken for prediction: %.2f sec' % (end_predict_time - start_predict_time))
 
-    # TODO: Detect the objects in `img` using the loaded model
-    #
-    # ???
-    #
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy()
+                  for key, value in detections.items()}
+    detections['num_detections'] = num_detections
 
-    # TODO: Add bounding boxes and labels of the detected objects to `img` using
-    #  `viz_utils.visualize_boxes_and_labels_on_image_array(..)`. Tipps: All required data is either already
-    #  available or contained in the dict-like structure returned when applying the model to `img`. TensorFlow models
-    #  return tensors with an additional batch dimension that is not required for visualitation, i.e. take only
-    #  index[0] and convert it to a numpy array. TensorFlow returns detected classes as floats, but the visualization
-    #  requires ints.
-    #
-    # ???
-    #
+    # detection_classes should be ints.
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+    img = tf.squeeze(img)
 
-    imgs.append(img)
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+        image,
+        detections['detection_boxes'],
+        detections['detection_classes'],
+        detections['detection_scores'],
+        category_index,
+        use_normalized_coordinates=True,
+        max_boxes_to_draw=200,
+        min_score_thresh=.30,
+        agnostic_mode=False)
+    imgs.append(image)
 plt.figure(figsize=(13, 5))
 showImages(imgs)
